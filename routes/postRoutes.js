163e5dcfb5d0a316c776a036437dbacf8fa1a3b2
TANUJADEVIDADISETTY <dtanuja3011@gmail.com>
2025-12-09 15:39:40 +0530

    routes folder is added

---
+++ routes/postRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const postController = require("../controllers/postController");
+
+router.post("/", postController.createPost);
+router.get("/", postController.getPosts);
+router.get("/:id", postController.getPostById);
+router.put("/:id", postController.updatePost);
+router.delete("/:id", postController.deletePost);
+
+// nested
+router.get("/author/:id/posts", postController.getAuthorPosts);
+
+module.exports = router;
